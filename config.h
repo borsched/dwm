08a6553d3d87a65a3ee94b7c14c60a092dc13567
borsched <borsched@github.com>
2020-12-16 11:31:37 -0600

    Autostart and vanity gaps patches

--- config.h
+++ config.h
@@ -5,6 +5,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font Mono:size=10" };
@@ -74,6 +79,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_o,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_x,      incrgaps,       {.i = +3 } },
+	{ MODKEY|ShiftMask,             XK_x,      incrgaps,       {.i = -3 } },
+	{ MODKEY,                       XK_a,      togglegaps,     {0} },
+	{ MODKEY|ShiftMask,             XK_a,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
@@ -82,7 +91,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,             XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
@@ -99,10 +108,10 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	/* { MODKEY|ShiftMask,             XK_q,      quit,           {0} }, */
-	{ MODKEY,						XK_w,		spawn,			SHCMD("firefox")	},
-	{ MODKEY,						XK_e,		spawn,			SHCMD("chromium")	},
-	{ MODKEY,						XK_p,		spawn,			SHCMD("pavucontrol")},
-	{ MODKEY,						XK_z,		spawn,			SHCMD(TERMINAL " -e ~/zoom-joiner/zoom.py")},
+	{ MODKEY,                       XK_w,       spawn,         SHCMD("firefox")	},
+	{ MODKEY,                       XK_e,       spawn,         SHCMD("chromium")	},
+	{ MODKEY,                       XK_p,       spawn,         SHCMD("pavucontrol")},
+	{ MODKEY,                       XK_z,       spawn,         SHCMD(TERMINAL " -e ~/zoom-joiner/zoom.py")},
 };
 
 /* button definitions */
